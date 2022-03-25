bae25278c5f2a216642c8a63a83c94f16cdf54c0
dpreid04 <98857427+dpreid04@users.noreply.github.com>
2022-03-24 20:53:24 -0500

    Update GamepadMultiplayer.java

--- GamepadMultiplayer.java
+++ GamepadMultiplayer.java
@@ -20,6 +20,7 @@ public class GamepadMultiplayer extends LinearOpMode {
     private DcMotor omniLeft = null;
     private DcMotor omniRight = null;
     private DcMotor arm = null;
+    private DcMotor arm2 = null;
     private DcMotor carousel = null;
     private Servo claw = null;
     private Servo claw2 = null;
@@ -29,28 +30,27 @@ public class GamepadMultiplayer extends LinearOpMode {
         telemetry.addData("Status", "Initialized");
         telemetry.update();
 
-        // Initialize the hardware variables. Note that the strings used here as parameters
-        // to 'get' must correspond to the names assigned during the robot configuration
-        // step (using the FTC Robot Controller app on the phone).
+        // Initialize the hardware variables.
         driveLeft  = hardwareMap.get(DcMotor.class, "drive_left");
         driveRight = hardwareMap.get(DcMotor.class, "drive_right");
         omniLeft  = hardwareMap.get(DcMotor.class, "omni_left");
         omniRight = hardwareMap.get(DcMotor.class, "omni_right");
         arm = hardwareMap.get(DcMotor.class, "arm");
+        arm2 = hardwareMap.get(DcMotor.class, "arm2");
         carousel = hardwareMap.get(DcMotor.class, "carousel");
-        //claw = hardwareMap.get(Servo.class, "claw");
+        claw = hardwareMap.get(Servo.class, "claw");
         claw2 = hardwareMap.get(Servo.class, "claw2");
 
 
 
-        // Most robots need the motor on one side to be reversed to drive forward
-        // Reverse the motor that runs backwards when connected directly to the battery
+        //set motor directions
         
         driveLeft.setDirection(DcMotor.Direction.FORWARD);
         driveRight.setDirection(DcMotor.Direction.REVERSE);
         omniLeft.setDirection(DcMotor.Direction.FORWARD);
         omniRight.setDirection(DcMotor.Direction.REVERSE);
         arm.setDirection(DcMotor.Direction.REVERSE);
+        arm2.setDirection(DcMotor.Direction.FORWARD);
         carousel.setDirection(DcMotor.Direction.FORWARD);
 
 
@@ -61,17 +61,19 @@ public class GamepadMultiplayer extends LinearOpMode {
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
 
-            // Setup a variable for each drive wheel to save power level for telemetry
+            // Setup variables for each system to save values for telemetry
             double leftPower;
             double rightPower;
             double armPower = 0;
-
-            // POV Mode uses left stick to go forward, and right stick to turn.
+            boolean clawOpen;
+            double MaxSpd = 0.75;
+            
+            // left stick to go forward, and right stick to turn.
             double drive = -gamepad1.left_stick_y;
             double turn  =  gamepad1.right_stick_x;
             leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
             rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
-            
+
             // Send calculated power to wheels
             driveLeft.setPower(leftPower);
             driveRight.setPower(rightPower);
@@ -82,16 +84,30 @@ public class GamepadMultiplayer extends LinearOpMode {
             //if "a" is pressed, grab Object
             if(gamepad2.a)
             {
-                claw2.setPosition(0.15);
+                claw2.setPosition(1);
                 
             }
             //if "b" is pressed, release object
             if(gamepad2.b)
             {
-                claw2.setPosition(0.9);
+                claw2.setPosition(0.6);
                 
             }
             
+            //if "x" is pressed, lift Object
+            if(gamepad2.x)
+            {
+                sleep(0);
+                claw.setPosition(0.0);
+                
+            }
+            //if "y" is pressed, lower Object
+            if(gamepad2.y)
+            {
+                sleep(0);
+                claw.setPosition(0.5);
+                
+            }
             
             //binds arm to triggers
             if(gamepad2.right_trigger <= 0.3)
@@ -106,32 +122,63 @@ public class GamepadMultiplayer extends LinearOpMode {
             
                
             //right trigger moves arm up
-            if(gamepad2.right_trigger >= 0.25)
+            if(gamepad2.right_trigger >= 0.25 && gamepad2.right_trigger < MaxSpd)
             {
-                armPower = gamepad1.right_trigger;
+                armPower = gamepad2.right_trigger;
                 arm.setPower(armPower);
             }
             else
             {
                 //left trigger moves arm down
-                if(gamepad2.left_trigger >= 0.25)
+                if(gamepad2.left_trigger >= 0.25 && gamepad2.right_trigger < MaxSpd)
                 {
-                    armPower = gamepad1.left_trigger;
+                    armPower = gamepad2.left_trigger;
                     arm.setPower(-armPower);
                 }
             }
-            
-            if(gamepad1.right_bumper||gamepad1.left_bumper)
+
+
+            //binds arm2 to triggers
+            if(gamepad2.right_trigger <= 0.3)
+            {
+                //sets a deadzone of 0.3, where the arm2 doesnt move at 0.3.
+                //if trigger is less than or equal to 0.3, keep the arm2 in place.
+
+                armPower = 0.08;
+                arm2.setPower(armPower);
+
+            }
+
+
+            //right trigger moves arm2 up
+            if(gamepad2.right_trigger >= 0.25 && gamepad2.right_trigger < MaxSpd)
+            {
+                armPower = gamepad2.right_trigger;
+                arm2.setPower(armPower);
+            }
+            else
+            {
+                //left trigger moves arm2 down
+                if(gamepad2.left_trigger >= 0.25 && gamepad2.right_trigger < MaxSpd)
+                {
+                    armPower = gamepad2.left_trigger;
+                    arm2.setPower(-armPower);
+                }
+            }
+
+            if(gamepad2.right_bumper||gamepad2.left_bumper)
             {
                 carousel.setPower(1);
                 sleep(1000);
-                carousel.setPower(0);                                
-            }
+                carousel.setPower(0);
+            }            
             
-            
-            // Show the elapsed game time and wheel power.
+            // Show the elapsed game time, wheel power, intake state, and arm power.
             telemetry.addData("Status", "Run Time: " + runtime.toString());
             telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
+            telemetry.addData("lift", "arm (%.2f)", armPower);
+            
+
             telemetry.update();
         }
     }
